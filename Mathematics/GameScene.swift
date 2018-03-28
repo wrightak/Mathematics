88b79e0ff5b0c7761b6a0a18c19ac43c415d3229
Andrew Wright <wrightak@gmail.com>
2018-03-29 06:15:31 +0900

    Initial progress

--- Mathematics/GameScene.swift
+++ Mathematics/GameScene.swift
@@ -1,8 +1,8 @@
 //
 //  GameScene.swift
-//  Mathematics
+//  test
 //
-//  Created by Andrew Wright on 29/03/2018.
+//  Created by Andrew Wright on 19/03/2018.
 //  Copyright © 2018 Andrew Wright. All rights reserved.
 //
 
@@ -10,80 +10,125 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+  
+  private var label : SKLabelNode?
+  private var numberNode : SKShapeNode?
+  private var numberLabels: [SKLabelNode] = []
+  
+  private let instructions = [
+    "Hi! Welcome to Mathematics. Tap to continue.",
+    """
+1, 2, 3, 4, 5, 6, ...
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+are the natural numbers.
+"""
+  ]
+  
+  override func didMove(to view: SKView) {
+    self.label = SKLabelNode(text: instructions[0])
+    if let label = self.label {
+      label.numberOfLines = 0
+      label.preferredMaxLayoutWidth = self.size.width
+      label.text = instructions[0]
+      label.alpha = 0.0
+      label.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+      label.fontSize = 50.0
+      self.addChild(label)
+      label.run(SKAction.fadeIn(withDuration: 2.0))
+    }
+  }
+  
+  
+  func touchDown(atPoint pos : CGPoint) {
+  }
+  
+  func touchMoved(toPoint pos : CGPoint) {
+  }
+  
+  func touchUp(atPoint pos : CGPoint) {
+    guard let label = self.label else {
+      return
+    }
     
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+    let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+    label.run(fadeOut, completion: {
+      self.displayNumbers(upTo: 6)
+    })
+  }
+  
+  private func displayNumbers(upTo finalNumber: Int) {
+    let circleDiameter: CGFloat = 80.0
+    let circleRadius: CGFloat = circleDiameter / 2.0
+    let spacing: CGFloat = 20.0
+    let horizontalNodeCount: Int = Int(floor((self.size.width - spacing) / (circleDiameter + spacing)))
+    let verticalNodeCount: Int = 10
+    
+    var counter: Int = 0
+    
+    for y in 0..<verticalNodeCount {
+      for x in 0..<horizontalNodeCount {
+        guard counter < finalNumber else {
+          break
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        counter += 1
+        let node = SKLabelNode(text: "\(counter)")
+        var xPosition: CGFloat
+        var yPosition: CGFloat
+        if x == 0 {
+          xPosition = spacing + circleRadius
+        } else {
+          xPosition = spacing + circleRadius + (CGFloat(x) * (circleDiameter + spacing))
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        if y == 0 {
+          yPosition = self.size.height - (spacing + circleRadius)
+        } else {
+          yPosition = self.size.height - (spacing + circleRadius + (CGFloat(y) * (circleDiameter + spacing)))
+        }
+        
+        node.position = CGPoint(x: xPosition, y: yPosition)
+        node.alpha = 0.0
+        node.name = "numberLabel_\(counter)"
+        
+        numberLabels.append(node)
+        self.addChild(node)
+      }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    fadeInNumbersConsecutively(fromIndex: 0)
+  }
+  
+  func fadeInNumbersConsecutively(fromIndex index: Int) {
+    guard index < numberLabels.count else {
+      return
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    let fadeIn = SKAction.fadeIn(withDuration: 1.0)
+    numberLabels[index].run(fadeIn) {
+      self.fadeInNumbersConsecutively(fromIndex: index + 1)
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+  }
+  
+  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+  }
+  
+  
+  override func update(_ currentTime: TimeInterval) {
+    // Called before each frame is rendered
+  }
 }
+
